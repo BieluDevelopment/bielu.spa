166fd8f70c62ac0f3ee29704f1084a855e47d138
Arkadiusz Biel <abi@novicell.co.uk>
2022-12-26 15:54:07 +0000

    feat: implement theming

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 // https://astro.build/config
 import vue from "@astrojs/vue";
-
+import dynamicImport from 'vite-plugin-dynamic-import'
 // https://astro.build/config
 export default defineConfig({
   
@@ -11,4 +11,9 @@ export default defineConfig({
   }),
   integrations: [vue()],
   output: 'server',  
+  vite: {
+    plugins: [
+      dynamicImport()
+    ]
+  }
 });
